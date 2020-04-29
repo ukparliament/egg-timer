f31dfea0fd94776a74aae15b905803686bdf42d3
Michael Smethurst <michael@smethur.st>
2020-04-29 17:37:32 +0100

    added following parliament method

--- app/models/parliament_period.rb
+++ app/models/parliament_period.rb
@@ -12,7 +12,11 @@ class ParliamentPeriod < ActiveRecord::Base
     dates = ''
     dates = dates + self.start_on.strftime( '%-d %B %Y' )
     dates = dates + ' - '
-    dates = dates + self.start_on.strftime( '%-d %B %Y' ) if self.end_on
+    dates = dates + self.end_on.strftime( '%-d %B %Y' ) if self.end_on
     dates
+  end
+  
+  def following_parliament_period
+    ParliamentPeriod.all.where( "start_on > ?", self.end_on ).order( 'start_on' ).first
   end
 end
