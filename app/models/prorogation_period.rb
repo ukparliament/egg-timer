5faa086a3c1d98a514380c4e8a1891dba62eb1fa
Michael Smethurst <michael@smethur.st>
2020-04-30 10:11:24 +0100

    added title with parl and date methods

--- app/models/prorogation_period.rb
+++ app/models/prorogation_period.rb
@@ -1,4 +1,20 @@
 class ProrogationPeriod < ActiveRecord::Base
   
   belongs_to :parliament_period
+  
+  def label_with_parliament
+    label = 'Parliament '
+    label = label + self.parliament_period.number.to_s
+    label = label + ' Prorogation '
+    label = label + self.number.to_s
+    label
+  end
+  
+  def dates
+    dates = ''
+    dates = dates + self.start_on.strftime( '%-d %B %Y' )
+    dates = dates + ' - '
+    dates = dates + self.end_on.strftime( '%-d %B %Y' ) if self.end_on
+    dates
+  end
 end
