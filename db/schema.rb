4f2b5e4af6e603f1713ac3f64528f4c9c724622c
Robert Brook <robertbrook@fastmail.fm>
2020-05-01 11:52:34 +0100

    not sure why this is full of code

--- db/schema.rb
+++ db/schema.rb
@@ -16,4 +16,86 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "adjournment_days", force: :cascade do |t|
+    t.integer "session_id", null: false
+    t.integer "house_id",   null: false
+    t.integer "date_id",    null: false
+  end
+
+  create_table "dates", force: :cascade do |t|
+    t.date "date", null: false
+  end
+
+  create_table "dissolution_days", force: :cascade do |t|
+    t.integer "prorogation_period_id", null: false
+    t.integer "date_id",               null: false
+  end
+
+  create_table "dissolution_periods", force: :cascade do |t|
+    t.integer "number",   null: false
+    t.date    "start_on", null: false
+    t.date    "end_on"
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+  end
+
+  create_table "non_sitting_days", force: :cascade do |t|
+    t.integer "session_id", null: false
+    t.integer "house_id",   null: false
+    t.integer "date_id",    null: false
+  end
+
+  create_table "parliament_periods", force: :cascade do |t|
+    t.integer "number",   null: false
+    t.date    "start_on", null: false
+    t.date    "end_on"
+  end
+
+  create_table "prorogation_days", force: :cascade do |t|
+    t.integer "dissolution_period_id", null: false
+    t.integer "date_id",               null: false
+  end
+
+  create_table "prorogation_periods", force: :cascade do |t|
+    t.integer "number",               null: false
+    t.date    "start_on",             null: false
+    t.date    "end_on"
+    t.integer "parliament_period_id", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "number",               null: false
+    t.date    "start_on",             null: false
+    t.date    "end_on"
+    t.integer "parliament_period_id", null: false
+  end
+
+  create_table "sitting_dates", force: :cascade do |t|
+    t.integer "sitting_day_id", null: false
+    t.integer "date_id",        null: false
+  end
+
+  create_table "sitting_days", force: :cascade do |t|
+    t.integer "session_id", null: false
+    t.integer "house_id",   null: false
+  end
+
+  add_foreign_key "adjournment_days", "dates", name: "fk_date"
+  add_foreign_key "adjournment_days", "houses", name: "fk_house"
+  add_foreign_key "adjournment_days", "sessions", name: "fk_session"
+  add_foreign_key "dissolution_days", "dates", name: "fk_date"
+  add_foreign_key "dissolution_days", "prorogation_periods", name: "fk_prorogation_period"
+  add_foreign_key "non_sitting_days", "dates", name: "fk_date"
+  add_foreign_key "non_sitting_days", "houses", name: "fk_house"
+  add_foreign_key "non_sitting_days", "sessions", name: "fk_session"
+  add_foreign_key "prorogation_days", "dates", name: "fk_date"
+  add_foreign_key "prorogation_days", "dissolution_periods", name: "fk_dissolution_period"
+  add_foreign_key "prorogation_periods", "parliament_periods", name: "fk_parliament_period"
+  add_foreign_key "sessions", "parliament_periods", name: "fk_parliament_period"
+  add_foreign_key "sitting_dates", "dates", name: "fk_date"
+  add_foreign_key "sitting_dates", "sitting_days", name: "fk_sitting_day"
+  add_foreign_key "sitting_days", "houses", name: "fk_house"
+  add_foreign_key "sitting_days", "sessions", name: "fk_session"
 end
