35e28493187fbdb4ae9d8c77df4d3ef96b0e1ccd
Michael Smethurst <michael@smethur.st>
2020-05-07 12:22:25 +0100

    catches when start date is not in calendar

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -10,44 +10,50 @@ class CalculatorController < ApplicationController
     day_count = params["day-count"].to_i
     
     # Find the start date in the calendar date table - if exists
-    @start_date = CalendarDate.find_by_date( start_date )
+    if CalendarDate.find_by_date( start_date )
+      @start_date = CalendarDate.find_by_date( start_date )
     
-    # Calculate the anticipated end date for a Proposed Statutory Instrument (PNSI)
-    # PNSIs are always before both Houses
-    if procedure == 1
-      # Set day counts to 0 for both Houses. These will increment for sitting downs found in a House
-      commons_day_count = 0
-      lords_day_count = 0
+      # Calculate the anticipated end date for a Proposed Statutory Instrument (PNSI)
+      # PNSIs are always before both Houses
+      if procedure == 1
+        # Set day counts to 0 for both Houses. These will increment for sitting downs found in a House
+        commons_day_count = 0
+        lords_day_count = 0
       
-      # Count starts first joint sitting day after laying
-      # Find the first joint sitting day following the start date (laying date in this case) - if there is one
-      if @start_date.first_joint_sitting_date
-        @date = @start_date.first_joint_sitting_date
+        # Count starts first joint sitting day after laying
+        # Find the first joint sitting day following the start date (laying date in this case) - if there is one
+        if @start_date.first_joint_sitting_date
+          @date = @start_date.first_joint_sitting_date
       
-        # Count 10 Lords sitting days
-        # Count 10 Commons sitting days
-        # Keep looping through consecutive dates until both the Commons and the Lords have sat for the number of days set
-        # Which for a PNSI is 10
-        # Do this loop until it's counted at least 10 days in the commons and at least 10 days in the lords
-        while ( ( commons_day_count <= day_count ) and ( lords_day_count <= day_count ) ) do
-          # Go to next date if there is one
-          if @date.next_date
-            @date = @date.next_date
-          else
-            # If there isn't a next date show error message and stop looping
-            @error_message = "Ooops. We've run out of calendar."
-            break
-          end
+          # Count 10 Lords sitting days
+          # Count 10 Commons sitting days
+          # Keep looping through consecutive dates until both the Commons and the Lords have sat for the number of days set
+          # Which for a PNSI is 10
+          # Do this loop until it's counted at least 10 days in the commons and at least 10 days in the lords
+          while ( ( commons_day_count <= day_count ) and ( lords_day_count <= day_count ) ) do
+            # Go to next date if there is one
+            if @date.next_date
+              @date = @date.next_date
+            else
+              # If there isn't a next date show error message and stop looping
+              @error_message = "Ooops. We've run out of calendar."
+              break
+            end
         
-          # Add one to the House count if that House sat that day
-          lords_day_count +=1 if @date.is_lords_sitting_day?
-          commons_day_count+=1 if @date.is_commons_sitting_day?
-        end
+            # Add one to the House count if that House sat that day
+            lords_day_count +=1 if @date.is_lords_sitting_day?
+            commons_day_count+=1 if @date.is_commons_sitting_day?
+          end
       
-      # if there's no future joint sitting date raise an error
-      else
-        @error_message = "Ooops. We've run out of calendar."
+        # if there's no future joint sitting date raise an error
+        else
+          @error_message = "Ooops. We've run out of calendar."
+        end
       end
     end
+  
+  # If the start date can't be found
+  else
+    @error_message = "Ooops. We've run out of calendar."
   end
 end
