8c0900ee1b968558257f336cd33832bf17407d8e
Michael Smethurst <michael@smethur.st>
2020-05-12 21:30:48 +0100

    added label method

--- app/models/adjournment_day.rb
+++ app/models/adjournment_day.rb
@@ -2,4 +2,10 @@ class AdjournmentDay < ActiveRecord::Base
   
   belongs_to :session
   belongs_to :calendar_date
+  
+  def label
+    label = ''
+    label = label + self.calendar_date.date.strftime( '%-d %B %Y' )
+    label
+  end
 end
