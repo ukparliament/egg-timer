266bdf207fce4f9ceda8bb0f5f748f0c81c27fa6
Michael Smethurst <michael@smethur.st>
2020-05-14 09:56:54 +0100

    new methods for sitting and adjournment days in session:

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,12 @@ class House < ActiveRecord::Base
   
   has_many :sitting_days
   has_many :adjournment_days
+  
+  def sitting_days_in_session( session )
+    SittingDay.all.where( house_id: self.id ).where( session_id: session.id )
+  end
+  
+  def adjournment_days_in_session( session )
+    AdjournmentDay.all.where( house_id: self.id ).where( session_id: session.id )
+  end
 end
