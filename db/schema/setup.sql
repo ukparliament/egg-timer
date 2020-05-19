2640fcf7d79ec1c7c325d0ee1f7b3e0ff0af64e3
Michael Smethurst <michael@smethur.st>
2020-05-19 22:05:20 +0100

    new schema without calendar dates or sitting dates

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,6 +1,5 @@
 drop table if exists dissolution_days;
 drop table if exists prorogation_days;
-drop table if exists sitting_dates;
 drop table if exists adjournment_days;
 drop table if exists sitting_days;
 drop table if exists sessions;
@@ -8,27 +7,26 @@ drop table if exists prorogation_periods;
 drop table if exists parliament_periods;
 drop table if exists dissolution_periods;
 drop table if exists houses;
-drop table if exists calendar_dates;
 
 create table parliament_periods (
 	id serial,
 	number int not null,
-	start_on date not null,
-	end_on date null,
+	start_date date not null,
+	end_date date null,
 	primary key (id)
 );
 create table dissolution_periods (
 	id serial,
 	number int not null,
-	start_on date not null,
-	end_on date null,
+	start_date date not null,
+	end_date date null,
 	primary key (id)
 );
 create table prorogation_periods (
 	id serial,
 	number int not null,
-	start_on date not null,
-	end_on date null,
+	start_date date not null,
+	end_date date null,
 	parliament_period_id int not null,
 	constraint fk_parliament_period foreign key (parliament_period_id) references parliament_periods(id),
 	primary key (id)
@@ -36,8 +34,8 @@ create table prorogation_periods (
 create table sessions (
 	id serial,
 	number int not null,
-	start_on date not null,
-	end_on date null,
+	start_date date not null,
+	end_date date null,
 	parliament_period_id int not null,
 	constraint fk_parliament_period foreign key (parliament_period_id) references parliament_periods(id),
 	primary key (id)
@@ -47,13 +45,10 @@ create table houses (
 	name varchar(50) not null,
 	primary key (id)
 );
-create table calendar_dates (
-	id serial,
-	date date not null,
-	primary key (id)
-);
 create table sitting_days (
 	id serial,
+	start_date date not null,
+	end_date date not null,
 	google_event_id varchar(255) not null,
 	session_id int not null,
 	house_id int not null,
@@ -63,40 +58,28 @@ create table sitting_days (
 );
 create table adjournment_days (
 	id serial,
+	date date not null,
 	google_event_id varchar(255) not null,
 	session_id int not null,
 	house_id int not null,
-	calendar_date_id int not null,
 	constraint fk_session foreign key (session_id) references sessions(id),
 	constraint fk_house foreign key (house_id) references houses(id),
-	constraint fk_calendar_date foreign key (calendar_date_id) references calendar_dates(id),
-	primary key (id)
-);
-create table sitting_dates (
-	id serial,
-	google_event_id varchar(255) not null,
-	sitting_day_id int not null,
-	calendar_date_id int not null,
-	constraint fk_sitting_day foreign key (sitting_day_id) references sitting_days(id),
-	constraint fk_calendar_date foreign key (calendar_date_id) references calendar_dates(id),
 	primary key (id)
 );
 create table prorogation_days (
 	id serial,
+	date date not null,
 	google_event_id varchar(255),
 	prorogation_period_id int not null,
-	calendar_date_id int not null,
 	constraint fk_prorogation_period foreign key (prorogation_period_id) references prorogation_periods(id),
-	constraint fk_calendar_date foreign key (calendar_date_id) references calendar_dates(id),
 	primary key (id)
 );
 create table dissolution_days (
 	id serial,
+	date date not null,
 	google_event_id varchar(255),
 	dissolution_period_id int not null,
-	calendar_date_id int not null,
 	constraint fk_dissolution_period foreign key (dissolution_period_id) references dissolution_periods(id),
-	constraint fk_calendar_date foreign key (calendar_date_id) references calendar_dates(id),
 	primary key (id)
 );
 
