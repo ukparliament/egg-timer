22e1fb2db2e3a919be0415681bf2f6cfb5ba81c8
Michael Smethurst <michael@smethur.st>
2020-05-19 22:31:29 +0100

    adapted to new schema. start date not date etc

--- app/models/parliament_period.rb
+++ app/models/parliament_period.rb
@@ -1,7 +1,7 @@
 class ParliamentPeriod < ActiveRecord::Base
   
-  has_many :sessions, -> { order( 'start_on desc' ) }
-  has_many :prorogation_periods, -> { order( 'start_on desc' ) }
+  has_many :sessions, -> { order( 'start_date desc' ) }
+  has_many :prorogation_periods, -> { order( 'start_date desc' ) }
   
   def label
     label = self.number.ordinalize + " Parliament"
@@ -10,13 +10,13 @@ class ParliamentPeriod < ActiveRecord::Base
   
   def dates
     dates = ''
-    dates = dates + self.start_on.strftime( '%-d %B %Y' )
+    dates = dates + self.start_date.strftime( '%-d %B %Y' )
     dates = dates + ' - '
-    dates = dates + self.end_on.strftime( '%-d %B %Y' ) if self.end_on
+    dates = dates + self.end_date.strftime( '%-d %B %Y' ) if self.end_date
     dates
   end
   
   def following_parliament_period
-    ParliamentPeriod.all.where( "start_on > ?", self.end_on ).order( 'start_on' ).first
+    ParliamentPeriod.all.where( "start_date > ?", self.end_date ).order( 'start_date' ).first
   end
 end
