f8f991031d9510f32dd426cfbb9446c3fd84f42e
Michael Smethurst <michael@smethur.st>
2020-05-19 22:32:22 +0100

    adapted to new schema. start date not date etc

--- app/models/dissolution_period.rb
+++ app/models/dissolution_period.rb
@@ -8,9 +8,9 @@ class DissolutionPeriod < ActiveRecord::Base
   
   def dates
     dates = ''
-    dates = dates + self.start_on.strftime( '%-d %B %Y' )
+    dates = dates + self.start_date.strftime( '%-d %B %Y' )
     dates = dates + ' - '
-    dates = dates + self.end_on.strftime( '%-d %B %Y' ) if self.end_on
+    dates = dates + self.end_date.strftime( '%-d %B %Y' ) if self.end_date
     dates
   end
 end
