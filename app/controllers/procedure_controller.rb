70449ae1ad503fa27fc708386ce59b77d71603c0
Michael Smethurst <michael@smethur.st>
2020-05-20 21:14:44 +0100

    acciendtlally deleted everything and checked in

--- app/controllers/procedure_controller.rb
+++ app/controllers/procedure_controller.rb
@@ -1,2 +1,25 @@
 class ProcedureController < ApplicationController
+  
+  def index
+    @title = 'Procedures'
+    @procedures = Procedure.all.order( 'display_order asc' )
+  end
+  
+  def show
+    procedure = params[:procedure]
+    @procedure = Procedure.find( procedure )
+    title = @procedure.name
+  end
+  
+  def flip
+    procedure = params[:procedure]
+    @procedure = Procedure.find( procedure )
+    if @procedure.active
+      @procedure.active = false
+    else
+      @procedure.active = true
+    end
+    @procedure.save
+    redirect_to :procedure_list
+  end
 end
