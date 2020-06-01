de8e0dbbe5a517ea8d4e5af6489fdb7adb98bd5b
Michael Smethurst <michael@smethur.st>
2020-06-01 11:11:12 +0100

    new virtual sitting day model

---
+++ app/models/virtual_sitting_day.rb
@@ -0,0 +1,14 @@
+class VirtualSittingDay < ActiveRecord::Base
+  
+  belongs_to :session
+  
+  def label
+    label = ''
+    label = label + self.start_date.strftime( '%-d %B %Y' )
+    if self.end_date != self.end_date
+      label = label + ' - '
+      label = label + self.end_date.strftime( '%-d %B %Y' )
+    end
+    label
+  end
+end
