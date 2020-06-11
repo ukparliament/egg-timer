be7036bfe50abc7b991d732422cd841400c58ebc
Michael Smethurst <michael@smethur.st>
2020-06-11 14:35:24 +0100

    moved pnsi controller into method

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -45,46 +45,12 @@ class CalculatorController < ApplicationController
       
       # Calculate the **anticipated end date** for a Proposed Statutory Instrument (PNSI):
       elsif @procedure.id == 3
+        
+        #### call calculation style 2
+        bicameral_first_to_ten_calculation( @start_date, @day_count )
+        
     
-        # We start counting on the **first day when both Houses are sitting** after the instrument is laid.
-        # If we find the **first joint sitting day** following the start date, the laying date in this case, ...
-        if @start_date.next_day.first_joint_parliamentary_sitting_day
-          @clock_date = @start_date.next_day.first_joint_parliamentary_sitting_day
         
-
-        	# PNSIs are always before both Houses, so we'll get ready to start counting the sitting days in each House.
-          # The first joint sitting day counts as day 1, so we count from 1, not 0
-          commons_day_count = 1
-          lords_day_count = 1
-          
-          # ... we look at subsequent days, ensuring that we've counted at least the set number of sitting days to count in each House. In the case of a PNSI, that's ten days.
-          while ( ( commons_day_count < @day_count ) and ( lords_day_count < @day_count ) ) do
-      
-            # Go to the **next day**
-            @clock_date = @clock_date.next_day
-    	
-            # PNSIs use parliamentary sitting days and no naive calendar days
-            # If the Lords sat on the date we've found, we add another day to the count.
-            lords_day_count +=1 if @clock_date.is_lords_parliamentary_sitting_day?
-            # If the Commons sat on the date we've found, we add another day to the count.
-            commons_day_count+=1 if @clock_date.is_commons_parliamentary_sitting_day?
-        
-            # Stop looping if the date is not a sitting day, not an adjournment day, not a prorogation day and not a dissolution day
-            # If we have no record for this day yet, we can't calculate the end date - and we show an error message.
-            if @clock_date.is_unannounced?
-              
-              # This error message is displayed to users.
-              @error_message = "It's not currently possible to calculate an anticipated end date, as the likely end date occurs during a period for which sitting days are yet to be announced."
-              break
-            end
-          end
-    	
-  	# If we didn't find any **future joint sitting date** in our calendar, we can't calculate the scrutiny period - and we show an error message.
-        else
-          
-          # This error message is displayed to users.
-          @error_message = "Unable to find a future joint sitting day. It's not currently possible to calculate an anticipated end date, as the likely end date occurs during a period for which sitting days are yet to be announced."
-        end
         
 
 
@@ -278,19 +244,68 @@ def bicameral_parliamentary_days_calculation( clock_date, target_day_count )
 end
 
 # Calculation style 2
+# A method for calculating committee scrutiny periods for PNSIs
+# Based on parliamentary sitting days
+# Counted from first joint sitting day, then last House to 10
+def bicameral_first_to_ten_calculation( clock_date, target_day_count )
+  
+    # We start counting on the **first day when both Houses are sitting** after the instrument is laid.
+    # If we find the **first joint sitting day** following the start date, the laying date in this case, ...
+    if clock_date.next_day.first_joint_parliamentary_sitting_day
+      clock_date = clock_date.next_day.first_joint_parliamentary_sitting_day
+    
+
+    	# PNSIs are always before both Houses, so we'll get ready to start counting the sitting days in each House.
+      # The first joint sitting day counts as day 1, so we count from 1, not 0
+      commons_day_count = 1
+      lords_day_count = 1
+      
+      # ... we look at subsequent days, ensuring that we've counted at least the set number of sitting days to count in each House. In the case of a PNSI, that's ten days.
+      while ( ( commons_day_count < day_count ) and ( lords_day_count < day_count ) ) do
+  
+        # Go to the **next day**
+        clock_date = clock_date.next_day
+	
+        # PNSIs use parliamentary sitting days and no naive calendar days
+        # If the Lords sat on the date we've found, we add another day to the count.
+        lords_day_count +=1 if clock_date.is_lords_parliamentary_sitting_day?
+        # If the Commons sat on the date we've found, we add another day to the count.
+        commons_day_count+=1 if clock_date.is_commons_parliamentary_sitting_day?
+    
+        # Stop looping if the date is not a sitting day, not an adjournment day, not a prorogation day and not a dissolution day
+        # If we have no record for this day yet, we can't calculate the end date - and we show an error message.
+        if clock_date.is_unannounced?
+          
+          # This error message is displayed to users.
+          @error_message = "It's not currently possible to calculate an anticipated end date, as the likely end date occurs during a period for which sitting days are yet to be announced."
+          break
+        end
+      end
+	
+# If we didn't find any **future joint sitting date** in our calendar, we can't calculate the scrutiny period - and we show an error message.
+    else
+      
+      # This error message is displayed to users.
+      @error_message = "Unable to find a future joint sitting day. It's not currently possible to calculate an anticipated end date, as the likely end date occurs during a period for which sitting days are yet to be announced."
+    end
+    @clock_date = clock_date
+  end
+end
+
+# Calculation style 3
 # A method for calculating based on "bums on seats" in Commons only
 # Used for treaty period B
 
-# Calculation style 3
+# Calculation style 4
 # Used for Commons only negative SIs
 # Counts through short adjournments (not bums on seats)
 
-# Calculation style 4
+# Calculation style 5
 # Used for Commons and Lords negative SIs and some Commons and Lords affirmative SIs
 # Counts when Commons OR Lords are sitting
 # Counts through short adjournments (not bums on seats)
 
-# Calculation style 5
+# Calculation style 6
 # Used for some Commons and Lords affirmative SIs
 # Counts when Commons AND Lords are sitting
 # Counts through short adjournments (not bums on seats)
