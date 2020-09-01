7ad0608139b0f7d1161f04a48f119ad1e84292cc
Michael Smethurst <michael@smethur.st>
2020-09-01 15:28:13 +0100

    fixed comments with robert

--- app/controllers/calculations/bicameral_praying_days_either_house_sitting.rb
+++ app/controllers/calculations/bicameral_praying_days_either_house_sitting.rb
@@ -1,50 +1,58 @@
 module CALCULATION_BICAMERAL_PRAYING_DAYS_EITHER_HOUSE_SITTING
   
-  # Used for Commons and Lords negative SIs and some Commons and Lords affirmative SIs
-  # Counts when Commons OR Lords are sitting
-  # Counts through short adjournments (not bums on seats)
+  # # A method for calculating scrutiny periods during which **either** House must be sitting or on a short adjournment, used for Commons and Lords negative Statutory Instruments and made affirmative SIs where this is set out by their enabling Act.
+  # The calculation counts a day whenever either House has a praying day, requiring the laying date and the number of days to count.
   def bicameral_praying_days_calculation_either_house_sitting( date, target_day_count )
-  
-    # Get ready to count praying days in both Houses
-    # If this is not a praying day for the Commons or the Lords...
-    unless date.is_commons_praying_day? or date.is_lords_praying_day?
     
-      # If there is a future praying day in the Commons or the Lords
+    
+    # ## We start counting on the **first day when either House has a praying day**.
+    # This may include the laying day of the instrument.
+    # Unless the laying day is a praying day in either House, then ...
+    unless date.is_commons_praying_day? or date.is_lords_praying_day?
+      
+      # ... if there is a future praying day in either House ...
       if date.first_praying_day_in_either_house
       
-        # Set the date to the first praying day in the Commons or the Lords
+        # ... we set the date to that day.
         date = date.first_praying_day_in_either_house 
       
-      # If we didn't find any **future praying day* in our calendar, we can't calculate the scrutiny period - and we show an error message and stop running this code.
+      # If we didn't find a **future praying day in either House** in our calendar, we can't calculate the scrutiny period, ...
       else
   
-        # This error message is displayed to users.
+        # ... this error message is displayed ...
         @error_message = "Unable to find a future praying day in the House of Commons or the House of Lords. It's not currently possible to calculate an anticipated end date, as the likely end date occurs during a period for which sitting days are yet to be announced."
+        
+        # ... and we stop looking for a scrutiny period end date.
         return
       end
+      
+    # Otherwise, we've established the laying day is a praying day in at least one House, so we don't have to cycle through the calendar to find a subsequent one.
     end
     
-    # We've found the first praying day so start from 1
+    # We've found the first praying day in either House so we start counting from day 1.
     day_count = 1
-  
-    # ... we look at each of our calendar dates, ensuring that we've counted the set number of days to count.
+    
+    # ## Whilst the number of praying days we’re counting is less than the target number of days to count ...
     while ( day_count < target_day_count ) do
     
-      # Skip to the next calendar day
+      # ... continue to the **next day**.
       date = date.next_day
     
-      # If the date we've found was either a Commons or a Lords praying day, we add another day to the count.
+      # ... and add 1 to the day count if this is a praying day in either House.
       day_count +=1 if date.is_either_house_praying_day?
-    
-      # Stop looping if the date is not a sitting day, not an adjournment day, not a prorogation day and not a dissolution day
-      # If we have no record for this day yet, we can't calculate the end date - and we show an error message.
+      
+      # ... if the calendar has no record of what type of day this is, we can't calculate the end date, ...
       if date.is_calendar_not_populated?
+        
+        # ... this error message is displayed to users ...
         @error_message = "It's not currently possible to calculate an anticipated end date, as the likely end date occurs during a period for which sitting days are yet to be announced."
+        
+        # ... and we stop looking through the calendar.
         break
       end
     end
   
-    # Return date for display on page
+     # Return the anticipated end date of the scrutiny period for display.
     date
   end
 end
