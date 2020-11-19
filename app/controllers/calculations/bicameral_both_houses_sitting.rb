654081037aa364d96f7c77e35b3193d55f8e99a0
Michael Smethurst <michael@smethur.st>
2020-11-19 17:05:23 +0000

    updated comments to take account of made things starting from making

--- app/controllers/calculations/bicameral_both_houses_sitting.rb
+++ app/controllers/calculations/bicameral_both_houses_sitting.rb
@@ -2,7 +2,7 @@ module CALCULATION_BICAMERAL_BOTH_HOUSES_SITTING
   
   # # A method for calculating the end date of scrutiny periods during which **both** Houses must be sitting or on a short adjournment, used for made affirmative Statutory Instruments where this is set out by their enabling Act. Also used for Legislative Reform Orders, Public Body Orders and Localism Orders.
   # The Statutory Instrument Act 1946 [sets out](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-1) that - in calculating the scrutiny period for an SI under that Act - “no account shall be taken of any time during which Parliament is dissolved or prorogued or during which *both* Houses are adjourned for more than four days.” This applies to the majority of made affirmative instruments.
-  # Made affirmatives laid under different Acts follow other rules, for example: the National Insurance Contributions Act 2014 [sets out](https://www.legislation.gov.uk/ukpga/2014/7/section/5/enacted#section-5-9), “no account is to be taken of any time ... during which either House is adjourned for more than four days.
+  # Made affirmatives laid under different Acts follow other rules, for example: the National Insurance Contributions Act 2014 [sets out](https://www.legislation.gov.uk/ukpga/2014/7/section/5/enacted#section-5-9), “no account is to be taken of any time [..] during which either House is adjourned for more than 4 days."
   # This calculation deals with the case where days are not counted if *either* House is adjourned for more than four days.
   # The rules governing the time period for Legislative Reform Orders are set out in the [Legislative and Regulatory Reform Act 2006 section 19](https://www.legislation.gov.uk/ukpga/2006/51/section/19#section-19).
   # The rules governing the time period for Public Body Orders are set out in the [Public Bodies Act 2011 section 11 (12)](https://www.legislation.gov.uk/ukpga/2011/24/section/11#section-11-12).
@@ -11,14 +11,16 @@ module CALCULATION_BICAMERAL_BOTH_HOUSES_SITTING
   def bicameral_calculation_both_houses_sitting( date, target_day_count )
     
     # ## We start counting on the **first day both Houses have a scrutiny day**.
-    # This will be the day on which the instrument was laid, if that day is a scrutiny day. For instruments covered by this calculation, lacking explicit instructions in enabling Acts, we decide to take the definition from the [Statutory Instruments Act 1946 Section 5 paragraph 1](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/5#section-5-1).
-    # If the laying day is a joint scrutiny day, we don't have to cycle through the calendar to find a subsequent one ...
+    # For Legislative Reform Orders, Public Body Orders and Localism Orders this will be the day on which the instrument was laid, if that day was a scrutiny day.
+    # For made affimatives this will be the day on which the instrument was made, if that day was a scrutiny day.
+    # For instruments covered by this calculation, lacking explicit instructions in enabling Acts, we decide to take the definition from the [Statutory Instruments Act 1946 Section 5 paragraph 1](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/5#section-5-1).
+    # If the laying day - or making day for a made affirmative - is a joint scrutiny day, we don't have to cycle through the calendar to find a subsequent one ...
     if date.is_joint_scrutiny_day?
       
-      # ... and the laying day is the start of the scrutiny period.
+      # ... and the laying or making day is the start of the scrutiny period.
       @scrutiny_start_date = date
       
-    # Otherwise, the laying day is not a joint scrutiny day, then ...
+    # Otherwise, the laying or making day is not a joint scrutiny day, then ...
     else
       
       # ... if there is a future joint scrutiny day ...
--- public/bicameral_both_houses_sitting.rb.md
+++ public/bicameral_both_houses_sitting.rb.md
@@ -3,7 +3,7 @@ module CALCULATION_BICAMERAL_BOTH_HOUSES_SITTING
 
 The Statutory Instrument Act 1946 [sets out](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-1) that - in calculating the scrutiny period for an SI under that Act - “no account shall be taken of any time during which Parliament is dissolved or prorogued or during which *both* Houses are adjourned for more than four days.” This applies to the majority of made affirmative instruments.
 
-Made affirmatives laid under different Acts follow other rules, for example: the National Insurance Contributions Act 2014 [sets out](https://www.legislation.gov.uk/ukpga/2014/7/section/5/enacted#section-5-9), “no account is to be taken of any time ... during which either House is adjourned for more than four days.
+Made affirmatives laid under different Acts follow other rules, for example: the National Insurance Contributions Act 2014 [sets out](https://www.legislation.gov.uk/ukpga/2014/7/section/5/enacted#section-5-9), “no account is to be taken of any time [..] during which either House is adjourned for more than 4 days."
 
 This calculation deals with the case where days are not counted if *either* House is adjourned for more than four days.
 
@@ -16,15 +16,19 @@ The rules governing the time period for Localism Orders are set out in the [Loca
   def bicameral_calculation_both_houses_sitting( date, target_day_count )
 ## We start counting on the **first day both Houses have a scrutiny day**.
 
-This will be the day on which the instrument was laid, if that day is a scrutiny day. For instruments covered by this calculation, lacking explicit instructions in enabling Acts, we decide to take the definition from the [Statutory Instruments Act 1946 Section 5 paragraph 1](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/5#section-5-1).
+For Legislative Reform Orders, Public Body Orders and Localism Orders this will be the day on which the instrument was laid, if that day was a scrutiny day.
 
-If the laying day is a joint scrutiny day, we don't have to cycle through the calendar to find a subsequent one ...
+For made affimatives this will be the day on which the instrument was made, if that day was a scrutiny day.
+
+For instruments covered by this calculation, lacking explicit instructions in enabling Acts, we decide to take the definition from the [Statutory Instruments Act 1946 Section 5 paragraph 1](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/5#section-5-1).
+
+If the laying day - or making day for a made affirmative - is a joint scrutiny day, we don't have to cycle through the calendar to find a subsequent one ...
 
     if date.is_joint_scrutiny_day?
-... and the laying day is the start of the scrutiny period.
+... and the laying or making day is the start of the scrutiny period.
 
       @scrutiny_start_date = date
-Otherwise, the laying day is not a joint scrutiny day, then ...
+Otherwise, the laying or making day is not a joint scrutiny day, then ...
 
     else
 ... if there is a future joint scrutiny day ...
