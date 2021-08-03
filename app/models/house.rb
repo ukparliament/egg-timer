3bea7a0aad9eeef78dca2ea204b7ca7ed5bd03d2
Michael Smethurst <michael@smethur.st>
2021-08-03 21:42:54 +0100

    methods for upcoming sitting days in a house and upcoming in a house

--- app/models/house.rb
+++ app/models/house.rb
@@ -15,4 +15,21 @@ class House < ActiveRecord::Base
   def adjournment_days_in_session( session )
     AdjournmentDay.all.where( house_id: self.id ).where( session_id: session.id ).order( 'date asc' )
   end
+  
+  def upcoming_sitting_days
+    SittingDay.find_by_sql(
+      "
+        SELECT start_date AS start_date, end_date AS end_date, 'sitting_day' AS day_type
+        FROM sitting_days
+        WHERE house_id = #{self.id}
+        AND start_date >= '#{Date.today}'
+        ORDER BY start_date ASC
+      "
+    )
+  end
+  
+  def upcoming
+    upcoming = self.upcoming_sitting_days
+    upcoming
+  end
 end
