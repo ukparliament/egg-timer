9c824237e2fb92450637763f4561644e029dd74a
Michael Smethurst <michael@smethur.st>
2022-06-05 15:08:08 +0100

    moved title to controller

--- app/controllers/house_days_controller.rb
+++ app/controllers/house_days_controller.rb
@@ -3,50 +3,60 @@ class HouseDaysController < ApplicationController
   def upcoming
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - upcoming'
   end
   
   def sitting_day_list
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - sitting days'
   end
   
   def sitting_day_upcoming
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - upcoming sitting days'
   end
   
   def sitting_day_next
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - next sitting day'
   end
   
   def virtual_sitting_day_list
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - virtual sitting days'
   end
   
   def virtual_sitting_day_upcoming
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - upcoming virtual sitting days'
   end
   
   def virtual_sitting_day_next
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - next virtual sitting day'
   end
   
   def adjournment_day_list
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - adjournment days'
   end
   
   def adjournment_day_upcoming
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - upcoming adjournment days'
   end
   
   def adjournment_day_next
     house = params[:house]
     @house = House.find( house )
+    @title = @house.name + ' - next adjournment day'
   end
 end
