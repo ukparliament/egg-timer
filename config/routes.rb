28186e1effbad200a0501cea56b85c9b1a89b2e5
Michael Smethurst <michael@smethur.st>
2022-09-02 12:32:41 +0100

    named home url

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'home#index', as: :home
   
   
   get 'parliament-periods' => 'parliament#index', as: :parliament_list
