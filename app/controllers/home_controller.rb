55567a20e587bc14b62e2d6e0ef95012eae9e343
Robert Brook <robertbrook@fastmail.fm>
2022-10-13 14:22:47 +0100

    lighter styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-  	@title = "Calculate scrutiny period"
+  	@title = "Calculate a scrutiny period"
     @procedures = Procedure.all.where( 'active is true' ).order( 'display_order asc')
   end
 end
