3f9e477960acb6fdc58c058fa49d3cbb2fe69b4b
Robert Brook <robertbrook@fastmail.fm>
2023-02-14 10:51:52 +0000

    routes magic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'egg-timer' => 'home#index', as: :home
+  get 'egg-timer' => 'home#index', as: :home
   
   
   get 'egg-timer/parliament-periods' => 'parliament#index', as: :parliament_list
