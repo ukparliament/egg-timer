4ca998f33370ff387ab7e90068c40c04a96efa95
fantasticlife <michael@smethur.st:wq>
2023-06-29 17:37:17 +0100

    config.active_record.legacy_connection_handling = false

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module ParliamentCalendar
     # Do not swallow errors in after_commit/after_rollback callbacks.
     #config.active_record.raise_in_transactional_callbacks = true
     
+    config.active_record.legacy_connection_handling = false
+    
 
     #config.autoloader = :classic
   end
