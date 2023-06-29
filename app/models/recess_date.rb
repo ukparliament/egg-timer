ea3d8ae45d275f56e911ed2867b4e2651eae88f2
fantasticlife <michael@smethur.st:wq>
2023-06-29 17:36:20 +0100

    added association to adjournment days

--- app/models/recess_date.rb
+++ app/models/recess_date.rb
@@ -1,6 +1,7 @@
 class RecessDate < ActiveRecord::Base
   
   belongs_to :house
+  has_many :adjournment_days
   
   def display_description
     self.description + ' ' + self.start_date.strftime( '%Y' )
