04e4a69397fc39344e344fa25b1cdf93190f8e63
fantasticlife <michael@smethur.st:wq>
2023-07-03 10:52:36 +0100

    render home index from / so i don't get baffled again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  get '/' => 'home#index'
 
   # You can have the root of your site routed with "root"
   get 'egg-timer' => 'home#index', as: :home
