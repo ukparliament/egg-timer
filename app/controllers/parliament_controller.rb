ea2252b95f7f90f018d5c8fb7df415db4448b111
fantasticlife <michael@smethur.st:wq>
2023-08-17 09:20:58 +0100

    moved in parliament period code out to parliament period model

--- app/controllers/parliament_controller.rb
+++ app/controllers/parliament_controller.rb
@@ -10,24 +10,7 @@ class ParliamentController < ApplicationController
     @parliament = ParliamentPeriod.find( parliament )
     @title = @parliament.label
     
-    # We construct a time periods array to store both sessions and prorogation periods.
-    @time_periods = []
-    @parliament.sessions.each do |session|
-      time_period =[]
-      time_period[0] = 'Session'
-      time_period[1]= session.start_date
-      time_period[2] = session
-      @time_periods << time_period
-    end
-    @parliament.prorogation_periods.each do |prorogation_period|
-      time_period =[]
-      time_period[0] = 'Prorogation'
-      time_period[1]= prorogation_period.start_date
-      time_period[2] = prorogation_period
-      @time_periods << time_period
-    end
-    
-    # We sort the time periods array by start date.
-    @time_periods.sort_by!{ |tp| tp[1] }
+    # We get all the periods in this Parliament, being sessions and prorogations.
+    @in_parliament_periods = @parliament.in_parliament_periods
   end
 end
