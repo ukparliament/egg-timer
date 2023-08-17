ce2f3563d8209314b86d0d9a7a47d893de4af808
fantasticlife <michael@smethur.st:wq>
2023-08-17 09:20:24 +0100

    added in_parliament_periods method

--- app/models/parliament_period.rb
+++ app/models/parliament_period.rb
@@ -19,4 +19,43 @@ class ParliamentPeriod < ActiveRecord::Base
   def following_parliament_period
     ParliamentPeriod.all.where( "start_date > ?", self.end_date ).order( 'start_date' ).first
   end
+  
+  # A method to get all periods within a Parliament, being sessions and prorogations.
+  def in_parliament_periods
+    
+    # We create an array to hold all periods in the Parliament.
+    in_parliament_periods = []
+    
+    # For each session in the Parlimant ...
+    self.sessions.each do |session|
+      
+      # ... we create an in Parliament period object ...
+      time_period = InParliamentPeriod.new
+      time_period.type = 'Session'
+      time_period.number = session.number
+      time_period.start_date = session.start_date
+      time_period.end_date = session.end_date
+      time_period.citation = session.citation
+      
+      # ... and add it to the array.
+      in_parliament_periods << time_period
+    end
+    
+    # For each prorogation in the Parlimant ...
+    self.prorogation_periods.each do |prorogation_period|
+      
+      # ... we create an in Parliament period object ...
+      time_period = InParliamentPeriod.new
+      time_period.type = 'Prorogation'
+      time_period.number = prorogation_period.number
+      time_period.start_date = prorogation_period.start_date
+      time_period.end_date = prorogation_period.end_date
+      
+      # ... and add it to the array.
+      in_parliament_periods << time_period
+    end
+    
+    # We sort the array by the start date of the period within the Parliament.
+    in_parliament_periods.sort_by!{ |ipp| ipp.start_date }
+  end
 end
