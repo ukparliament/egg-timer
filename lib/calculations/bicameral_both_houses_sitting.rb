21be70558b61ba71906a236c69c9891a33799d6d
Robert Brook <robertbrook@fastmail.fm>
2024-07-26 12:44:49 +0100

    Revert "added comments for enhanced affirmatives under the Investigatory Powers Act 2016"
    This reverts commit 717e963e94b9d3c3ecc9361f33435d51e9b1758b.

--- lib/calculations/bicameral_both_houses_sitting.rb
+++ lib/calculations/bicameral_both_houses_sitting.rb
@@ -1,6 +1,6 @@
 module CALCULATION_BICAMERAL_BOTH_HOUSES_SITTING
   
-  # # A method for calculating the end date of scrutiny periods during which **both** Houses must be sitting or on a short adjournment, used for made affirmative Statutory Instruments as set out by their enabling Act. Also used for Legislative Reform Orders, Public Body Orders, Localism Orders and enhanced affirmatives under the Investigatory Powers Act 2016.
+  # # A method for calculating the end date of scrutiny periods during which **both** Houses must be sitting or on a short adjournment, used for made affirmative Statutory Instruments as set out by their enabling Act. Also used for Legislative Reform Orders, Public Body Orders and Localism Orders.
   # [Paragraph 1 of section 7 of the Statutory Instrument Act 1946](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-1) sets out that - in calculating the scrutiny period for an SI under that Act - “no account shall be taken of any time during which Parliament is dissolved or prorogued or during which *both* Houses are adjourned for more than four days.” This applies to the majority of made affirmative instruments.
   # Made affirmatives laid under different Acts follow other rules, for example: [paragraph 9 of section 5 of the National Insurance Contributions Act 2014](https://www.legislation.gov.uk/ukpga/2014/7/section/5/enacted#section-5-9), sets out that “no account is to be taken of any time [..] during which either House is adjourned for more than 4 days."
   # This calculation deals with the case where days are not counted if *either* House is adjourned for more than four days.
@@ -8,12 +8,11 @@ module CALCULATION_BICAMERAL_BOTH_HOUSES_SITTING
   # The rules governing the time period for Public Body Orders are set out in [paragraph 12 of section 11 of the Public Bodies Act 2011](https://www.legislation.gov.uk/ukpga/2011/24/section/11#section-11-12).
   # The rules governing the time period for Localism Orders are set out in [paragraph 14 of section 19 of the Localism Act 2011](https://www.legislation.gov.uk/ukpga/2011/20/enacted#section-19-14).
   # The rules governing the time period for published drafts are set out in [paragraph 14 of schedule 8 of the European Union (Withdrawal) Act 2018](https://www.legislation.gov.uk/ukpga/2018/16/schedule/8/enacted#schedule-8-paragraph-14).
-  # The rules governing the time period for enhanced affirmatives under the [Investigatory Powers Act 2016](https://www.legislation.gov.uk/id/ukpga/2016/25/) are set out in [paragraph 11 of section 268 of the Investigatory Powers Act 2016](https://www.legislation.gov.uk/ukpga/2016/25/section/268#section-268-11).
   
   def bicameral_calculation_both_houses_sitting( date, target_day_count )
     
     # ## We start counting on the **first day both Houses have a scrutiny day**.
-    # For Legislative Reform Orders, Public Body Orders, Localism Orders and enhanced affirmatives under the Investigatory Powers Act 2016 this will be the day on which the instrument was laid, if that day was a scrutiny day.
+    # For Legislative Reform Orders, Public Body Orders and Localism Orders this will be the day on which the instrument was laid, if that day was a scrutiny day.
     # For published drafts under the European Union (Withdrawal) Act 2018, this will be the day on which the draft was published, if that day was a scrutiny day. This is set out in [paragraph 14(2) of Schedule 8 of the European Union (Withdrawal) Act 2018 ](https://www.legislation.gov.uk/ukpga/2018/16/schedule/8/enacted#schedule-8-paragraph-14-2).
     # For made affimatives this will be the day on which the instrument was made, if that day was a scrutiny day.
     # For instruments covered by this calculation, lacking explicit instructions in enabling Acts, we decide to take the definition from [paragraph 1 of section 5 of the Statutory Instruments Act 1946](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/5#section-5-1).
