9495d5214a766df84624b1e78b9ebd3e9c08f819
James Jefferies <james@shedcode.co.uk>
2024-12-05 10:23:02 +0000

    Remove unused files and spring

--- test/controllers/dissolution_period_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DissolutionPeriodControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/dissolution_period_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DissolutionPeriodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
