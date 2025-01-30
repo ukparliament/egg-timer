2b8a620d6ac62a87972e358bfb95d6f5100f1fbe
James Jefferies <james@shedcode.co.uk>
2025-01-30 17:37:28 +0000

    Add postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_token: ENV.fetch("POSTMARK_API_KEY") }
+
 end
