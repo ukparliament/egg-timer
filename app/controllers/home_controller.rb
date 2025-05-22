1a3f87b9bb6f3f1b52a7652d1ecfae7b98212e28
fantasticlife <michael@smethur.st>
2025-05-22 13:03:17 +0100

    title now set as pagetitle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-  	@title = "Calculate a scrutiny period"
+  	@page_title = "Calculate a scrutiny period"
     @procedures = Procedure.all.where( 'active is true' ).order( 'display_order asc')
   end
 end
