170279f2bb6ee9b4984d02b8f05baf202fa5cfdf
fantasticlife <michael@smethur.st>
2025-05-22 13:01:00 +0100

    application controller creates crumb holder as a before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  $SITE_TITLE = "Mr O'Common's Marvellous Mechanical Mouse Organ"
+
+  before_action do
+    create_crumb_container
+  end
+  
+  def create_crumb_container
+    @crumb = []
+  end
 end
