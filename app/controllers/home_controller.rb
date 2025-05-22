4208cee7b2ee384d61881f9f84f732be607c2aa6
fantasticlife <michael@smethur.st>
2025-05-22 20:54:56 +0100

    simlified home index action cos it's no longer the form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-  	@page_title = "Calculate a scrutiny period"
-    @procedures = Procedure.all.where( 'active is true' ).order( 'display_order asc')
+    @description = 'TODO'
   end
 end
