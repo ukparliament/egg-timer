058c847525bc059220e015a1e2bd8e51dd7a2734
fantasticlife <michael@smethur.st>
2025-05-27 16:45:09 +0100

    added page meta stuff to controller actions

--- app/controllers/dissolution_period_controller.rb
+++ app/controllers/dissolution_period_controller.rb
@@ -2,12 +2,25 @@ class DissolutionPeriodController < ApplicationController
   
   def index
     @dissolution_periods = DissolutionPeriod.all.order( 'number desc' )
-    @title = "Dissolution periods"
+
+    # Set a meta information for the page.
+    @page_title = "Dissolution periods"
+    @description = "Dissolution periods."
+    @crumb << { label: 'Time periods', url: parliamentary_time_list_url }
+    @crumb << { label: 'Dissolution periods', url: nil }
+    @section = 'time-periods'
   end
   
   def show
     dissolution_period = params[:dissolution_period]
     @dissolution_period = DissolutionPeriod.find( dissolution_period )
-    @title = @dissolution_period.label
+
+    # Set a meta information for the page.
+    @page_title = @dissolution_period.label
+    @description = "Dissolution periods."
+    @crumb << { label: 'Time periods', url: parliamentary_time_list_url }
+    @crumb << { label: 'Dissolution periods', url: dissolution_period_list_url }
+    @crumb << { label: @dissolution_period.label, url: nil }
+    @section = 'time-periods'
   end
 end
