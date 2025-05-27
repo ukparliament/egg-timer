0ae4c1778795b091c91ce366964cbc4394a2c941
fantasticlife <michael@smethur.st>
2025-05-27 16:47:47 +0100

    added page meta stuff to controller actions

--- app/controllers/parliamentary_time_controller.rb
+++ app/controllers/parliamentary_time_controller.rb
@@ -1,6 +1,12 @@
 class ParliamentaryTimeController < ApplicationController
 
   def index
-    @title = 'Parliamentary time'
+    @houses = House.all
+
+    # Set a meta information for the page.
+    @page_title = "Parliamentary time"
+    @description = "Parliamentary time."
+    @crumb << { label: 'Time periods', url: nil }
+    @section = 'time-periods'
   end
 end
