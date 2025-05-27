bba49d6a03955b8b4c8cebfcf37cee7365f4a134
fantasticlife <michael@smethur.st>
2025-05-27 16:48:02 +0100

    added page meta stuff to controller actions

--- app/controllers/prorogation_period_controller.rb
+++ app/controllers/prorogation_period_controller.rb
@@ -3,6 +3,13 @@ class ProrogationPeriodController < ApplicationController
   def index
     @prorogation_periods = ProrogationPeriod.all.order( 'start_date desc' )
     @title = "Prorogation periods"
+
+    # Set a meta information for the page.
+    @page_title = "Prorogation periods"
+    @description = "Prorogation periods."
+    @crumb << { label: 'Time periods', url: parliamentary_time_list_url }
+    @crumb << { label: 'Prorogation periods', url: nil }
+    @section = 'time-periods'
   end
   
   def show
@@ -23,5 +30,14 @@ class ProrogationPeriodController < ApplicationController
     @title = @prorogation_period.label_with_parliament
     @preceding_session = @prorogation_period.preceding_session
     @following_session = @prorogation_period.following_session
+
+    # Set a meta information for the page.
+    @page_title = @prorogation_period.label_with_parliament
+    @description = "#{@prorogation_period.label_with_parliament}."
+    @crumb << { label: 'Time periods', url: parliamentary_time_list_url }
+    @crumb << { label: 'Parliament periods', url: parliament_list_url }
+    @crumb << { label: @prorogation_period.parliament_label, url: parliament_show_url( :parliament => @prorogation_period.parliament_period_id )}
+    @crumb << { label: @prorogation_period.label_in_parliament, url: nil }
+    @section = 'time-periods'
   end
 end
