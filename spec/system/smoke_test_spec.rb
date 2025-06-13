ed08cd190532431b68375e4084470c6d8ad939e8
James Jefferies <james@shedcode.co.uk>
2025-06-13 10:50:14 +0100

    Fix smoke test

--- spec/system/smoke_test_spec.rb
+++ spec/system/smoke_test_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "smoke test", type: :system do
   it 'home page loads' do
     visit root_path
-    expect(page).to have_content("The Egg Timer")
+    expect(page).to have_content("egg timer")
   end
 end
