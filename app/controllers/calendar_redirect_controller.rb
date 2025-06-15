d9adb27e9ca31249fc6f6877641f6385844dea8e
fantasticlife <michael@smethur.st>
2025-06-15 21:52:23 +0100

    made calendar redirects to zero padded urls into 3012

--- app/controllers/calendar_redirect_controller.rb
+++ app/controllers/calendar_redirect_controller.rb
@@ -3,7 +3,7 @@ class CalendarRedirectController < ApplicationController
   def month
     month = params[:month]
     month = '0' + month if month.length == 1
-    redirect_to( calendar_month_url( :month => month ) )
+    redirect_to( calendar_month_url( :month => month ), :status => 301 )
   end
 
   def day
@@ -11,6 +11,6 @@ class CalendarRedirectController < ApplicationController
     month = '0' + month if month.length == 1
     day = params[:day]
     day = '0' + day if day.length == 1
-    redirect_to( calendar_day_url( :month => month, :day => day ) )
+    redirect_to( calendar_day_url( :month => month, :day => day ), :status => 301 )
   end
 end
