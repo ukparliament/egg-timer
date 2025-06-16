5000171b909767c65f6520d7b4b2f5024c0551f9
fantasticlife <michael@smethur.st>
2025-06-16 12:42:05 +0100

    updated title and description for parliamentary time index

--- app/controllers/parliamentary_time_controller.rb
+++ app/controllers/parliamentary_time_controller.rb
@@ -4,8 +4,8 @@ class ParliamentaryTimeController < ApplicationController
     @houses = House.all
 
     # Set a meta information for the page.
-    @page_title = "Parliamentary time"
-    @description = "Parliamentary time."
+    @page_title = "Time periods"
+    @description = "Parliamentary time periods."
     @crumb << { label: 'Time periods', url: nil }
     @section = 'time-periods'
   end
