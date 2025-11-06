a47be75522a42c43be0a2ce33a3e86dd9faf7c16
James Jefferies <james@shedcode.co.uk>
2025-11-06 14:11:01 +0000

    first batch of four questions including one without an end date

--- spec/system/calculations_spec.rb
+++ spec/system/calculations_spec.rb
@@ -16,23 +16,82 @@ describe "Calculation test 1", type: :system do
       SQL
     end
 
-    it 'calculates legislative reform order - negative procedure objection correctly' do
-      visit root_path
-      expect(page).to have_content("egg timer")
-      click_on "Scrutiny end date calculator"
+    context "Calculate end date when given start date" do
+      it "calculates 'legislative reform order - negative procedure objection' correctly" do
+        visit root_path
+        expect(page).to have_content("egg timer")
+        click_on "Scrutiny end date calculator"
 
-      choose "Negative statutory instrument praying period (laid before Commons only)"
+        choose "Negative statutory instrument praying period (laid before Commons only)"
 
-      fill_in 'start-date', with: "01/02/2024"
-      click_on "Continue with this period and starting date"
+        fill_in 'start-date', with: "01/02/2024"
+        click_on "Continue with this period and starting date"
 
-      expect(page).to have_content("Scrutiny end date - number of days to count required")
-      expect(page).to have_field('day-count', with: '40')
+        expect(page).to have_content("Scrutiny end date - number of days to count required")
+        expect(page).to have_field('day-count', with: '40')
 
-      click_on "Calculate"
+        click_on "Calculate"
 
-      expect(page).to have_content("The anticipated start date of the scrutiny period is Thursday, 1 February 2024")
-      expect(page).to have_content("The anticipated end date of the scrutiny period is Thursday, 21 March 2024")
+        expect(page).to have_content("The anticipated start date of the scrutiny period is Thursday, 1 February 2024")
+        expect(page).to have_content("The anticipated end date of the scrutiny period is Thursday, 21 March 2024")
+      end
+
+      it "calculates 'Made affirmative approval period (laid before both Houses, clock stops if either House rises)' correctly" do
+        visit root_path
+        expect(page).to have_content("egg timer")
+        click_on "Scrutiny end date calculator"
+
+        choose "Made affirmative approval period (laid before both Houses, clock stops if either House rises)"
+
+        fill_in 'start-date', with: "01/09/2019"
+        click_on "Continue with this period and starting date"
+
+        expect(page).to have_content("Scrutiny end date - number of days to count required")
+        expect(find_field('day-count').value).to be_nil
+        fill_in 'day-count', with: '28'
+
+        click_on "Calculate"
+
+        expect(page).to have_content("The anticipated start date of the scrutiny period is Tuesday, 3 September 2019")
+        expect(page).to have_content("The anticipated end date of the scrutiny period is Sunday, 20 October 2019")
+      end
+
+      it "calculates 'Proposed negative statutory instrument - committee sifting period' correctly" do
+        visit root_path
+        expect(page).to have_content("egg timer")
+        click_on "Scrutiny end date calculator"
+
+        choose "Proposed negative statutory instrument - committee sifting period"
+
+        fill_in 'start-date', with: "01/01/2025"
+        click_on "Continue with this period and starting date"
+
+        expect(page).to have_content("Scrutiny end date - number of days to count required")
+        expect(page).to have_field('day-count', with: '10')
+
+        click_on "Calculate"
+
+        expect(page).to have_content("The anticipated start date of the scrutiny period is Monday, 6 January 2025")
+        expect(page).to have_content("The anticipated end date of the scrutiny period is Monday, 20 January 2025")
+      end
+
+      it "cannot calculate 'Treaty objection period A' correctly as the calendar entries do not exist for it yet" do
+        visit root_path
+        expect(page).to have_content("egg timer")
+        click_on "Scrutiny end date calculator"
+
+        choose "Treaty objection period A"
+
+        fill_in 'start-date', with: "01/01/2030"
+        click_on "Continue with this period and starting date"
+
+        expect(page).to have_content("Scrutiny end date - number of days to count required")
+        expect(page).to have_field('day-count', with: '21')
+
+        click_on "Calculate"
+
+        expect(page).to have_content("Unable to find a future joint sitting day")
+      end
     end
   end
 end
