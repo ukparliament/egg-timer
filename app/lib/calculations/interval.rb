aefea42ec99db97c438cff89df05a1cb7de0f248
fantasticlife <michael@smethur.st>
2026-05-27 15:02:42 +0100

    interval calculator now reports if calendar is not populated

--- app/lib/calculations/interval.rb
+++ app/lib/calculations/interval.rb
@@ -11,23 +11,42 @@ module Calculations
 
       # Whilst the date is not later than the end date ...
       while date <= end_date
+      
+        #  ... if the calendar has no record of what type of day this is, we can't calculate the the number of sitting days, ...
+        if date.is_calendar_not_populated?
+        
+        puts "bugger"
 
-        # ... if the date is a sitting day in the Commons ...
-        if date.is_commons_parliamentary_sitting_day?
+          # ... this error message is displayed to users ...
+          @error_message = "It is not currently possible to calculate the number of sitting days, because the interval includes days for which the calendar is not populated."
+          
+          # ... and we stop looking through the calendar.
+          break
+          
+        # Otherwise, if the calendar does have a record of what type of day this is ...
+        else
+        
+        puts "good"
+          
+          # ... if the date is a sitting day in the Commons ...
+          if date.is_commons_parliamentary_sitting_day?
 
-          # ... we increment the Commons sitting day count.
-          @commons_sitting_day_count += 1
+            # ... we increment the Commons sitting day count.
+            @commons_sitting_day_count += 1
+          end
+
+          # If the date is a sitting day in the Lords ...
+          if date.is_lords_parliamentary_sitting_day?
+
+            # ... we increment the Lords sitting day count.
+            @lords_sitting_day_count += 1
+          end
+
+          # We continue to the **next day**.
+          date = date.next_day
+          
+          puts date
         end
-
-        # If the date is a sitting day in the Lords ...
-        if date.is_lords_parliamentary_sitting_day?
-
-          # ... we increment the Lords sitting day count.
-          @lords_sitting_day_count += 1
-        end
-
-        # We continue to the **next day**.
-        date = date.next_day
       end
     end
   end
