7aa3cd771bf20c73a2d79d599de0bf5513bb58c6
fantasticlife <michael@smethur.st>
2026-06-02 10:55:42 +0100

    refactored bicameral with either house sitting calculation

--- app/lib/calculations/backwards/bicameral_si_either_house_sitting.rb
+++ app/lib/calculations/backwards/bicameral_si_either_house_sitting.rb
@@ -3,18 +3,15 @@ module Calculations
     module BicameralSiEitherHouseSitting
 
       # # A method for calculating the start date of a scrutiny period, based on days on which **either** House must be sitting or on a short adjournment.
-      # This method is used for bicameral negative Statutory Instruments as set out by the Statutory Instruments Act 1946 and for made affirmative Statutory Instruments as set out by their enabling Act.
-      # The calculation is set out in [paragraph 1 Section 7 of the Statutory Instruments Act 1946](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-1), though a different calculation may be required if the instrument is laid under another Act - as per [paragraph 3](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-3).
-      # This method is also used for calculations of scrutiny periods for proposed draft remedial orders and draft affirmative remedial orders and for scrutiny and approval periods for made affirmative remedial orders as set out in [Schedule 2 of the Human Rights Act 1998](https://www.legislation.gov.uk/ukpga/1998/42/schedule/2#schedule-2).
-      # The calculation counts in actual sitting days, requiring the start date and the number of days to count.
-
-      def bicameral_si_either_house_sitting_calculation_backwards( date, target_day_count )
+      # This method is used for:
+      # * made affirmative Statutory Instruments as set out in their enabling Act, for example: [paragraph 4 of section 55 of the Sanctions and Anti-Money Laundering Act 2018](https://www.legislation.gov.uk/ukpga/2018/13/section/55#section-55-4)
+      # * bicameral negative Statutory Instruments as set out in [paragraph 1 Section 7 of the Statutory Instruments Act 1946](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-1)
+      # * proposed draft remedial orders, draft affirmative remedial orders and made affirmative remedial orders as set out in [Schedule 2 of the Human Rights Act 1998](https://www.legislation.gov.uk/ukpga/1998/42/schedule/2#schedule-2)
+      def bicameral_si_either_house_sitting_calculation_backwards( target_end_date, target_day_count )
       
-        # We set the scrutiny start date, being the start date of the calculation and the end date of the scrutiny period.
-        @scrutiny_start_date = nil
-      
-        # We go forward one day.
-        date = date.next
+        # Because the start of each loop causes the date to move to the preceding day and we wish to include the target end date ...
+        #  ... we go forward one day.
+        date = target_end_date.next
       
         # We set the day count to zero.
         day_count = 0
@@ -22,16 +19,20 @@ module Calculations
         # ## Whilst the number of days we’re counting is less than the target number of days to count ...
         while day_count < target_day_count
 
-          # ... we continue to the **previous day**.
+          # ... we move to the **previous day**.
           date = date.prev_day
         
           # If the day is a scrutiny day in either House ...
           if date.is_either_house_scrutiny_day?
           
-            # ... we set the scrutiny start date to this date if the scrutiny start date is nil ...
-            @scrutiny_start_date = date if @scrutiny_start_date.nil?
+            # ... if we've not set the scrutiny end date ...
+            unless @scrutiny_end_date
           
-            # ... and add 1 to the day count.
+              # ... we set the scrutiny end date to this date.
+              @scrutiny_end_date = date
+            end
+            
+            # We add 1 to the day count.
             day_count += 1
           
           # Otherwise, if the calendar has no record of what type of day this is, we can't calculate the end date, ...
