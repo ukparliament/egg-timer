2cd2133f764013f27bd990b650c7674a87c243e4
fantasticlife <michael@smethur.st>
2026-06-02 10:26:15 +0100

    refactored the commons only si calculation

--- app/lib/calculations/backwards/commons_only_si.rb
+++ app/lib/calculations/backwards/commons_only_si.rb
@@ -1,39 +1,39 @@
 module Calculations
   module Backwards
     module CommonsOnlySi
-      # # A method for calculating the end date of a scrutiny period, based on days on which the House of Commons must be sitting or on a short adjournment. This method is used for Commons only negative Statutory Instruments as set out by the Statutory Instruments Act 1946 and for made affirmative Statutory Instruments as set out by their enabling Act.
-      # The calculation counts in actual sitting days, requiring the start date and the number of days to count.
-      # The calculation is defined by paragraphs 1 and 2 of [Section 7 of the Statutory Instruments Act 1946](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7), though a different calculation may be required if the instrument is laid under another Act - as per [paragraph 3](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-3).
-
-      # date > target_end_date
-
-      def commons_only_si_calculation_backwards(date, target_day_count)
-        # ## We start counting on the **first day the House of Commons has a scrutiny day**.
-        # For negative Statutory Instruments this will be the day on which the instrument was laid, if that day was a scrutiny day. For made negative Statutory Instruments, this is defined by the [Statutory Instruments Act 1946 Section 5 paragraph 1](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/5#section-5-1). For draft negative Statutory Instruments, this is defined by the [Statutory Instruments Act 1946 Section 6 paragraph 1](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/6#section-6-1).
-        # For made affirmatives this will be the day on which the instrument was made, if that day was a scrutiny day.
+    
+      # # A method for calculating the start date of a scrutiny period, based on days on which the House of Commons must be sitting or on a short adjournment.
+      # This method is used for:
+      # * Commons only negative Statutory Instruments as set out by the Statutory Instruments Act 1946
+      # * Commons only made affirmative Statutory Instruments as set out by their enabling Act
+      # The calculation is set out in paragraphs 1 and 2 of [Section 7 of the Statutory Instruments Act 1946](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7).
+      # A different calculation may be required if the instrument is laid under another Act - as set out in [paragraph 3](https://www.legislation.gov.uk/ukpga/Geo6/9-10/36/section/7#section-7-3).
+      def commons_only_si_calculation_backwards( target_end_date, target_day_count )
       
-        # We set the scrutiny start date, being the start date of the calculation and the end date of the scrutiny period.
-        @scrutiny_start_date = nil
-      
-        # We go forward one day.
-        date = date.next
-      
+        # Because the start of each loop causes the date to move to the preceding day and we wish to include the target end date ...
+        #  ... we go forward one day.
+        date = target_end_date.next
+        
         # We set the day count to zero.
         day_count = 0
       
         # ## Whilst the number of days we’re counting is less than the target number of days to count ...
         while day_count < target_day_count
 
-          # ... we continue to the **previous day**.
+          # ... we move to the **previous day**.
           date = date.prev_day
         
           # If the day is a Commons scrutiny day ...
           if date.is_commons_scrutiny_day?
           
-            # ... we set the scrutiny start date to this date if the scrutiny start date is nil ...
-            @scrutiny_start_date = date if @scrutiny_start_date.nil?
+            # ... if we've not set the scrutiny end date ...
+            unless @scrutiny_end_date
+            
+              # ... we set the scrutiny end date to this date.
+              @scrutiny_end_date = date
+            end
           
-            # ... and add 1 to the day count.
+            # We add 1 to the day count.
             day_count += 1
           
           # Otherwise, if the calendar has no record of what type of day this is, we can't calculate the end date, ...
@@ -47,7 +47,7 @@ module Calculations
           end
         end
 
-        # Return the anticipated start date of the scrutiny period for display.
+        # We return the anticipated start date of the scrutiny period for display.
         date
       end
     end
